be51c3290422e726fd668d261440580da31e649a
Anush <movsesyan.anush@gmail.com>
2017-01-19 00:32:58 +0400

    Commit 2

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -8,84 +8,17 @@ namespace Test
 {
     class Program
     {
-        static void Main()
+        static void Main(string [] args)
         {
-            String s = "Uraa";
-
-            s = s.Replace("A", "Ա");
-            s = s.Replace("B", "Բ");
-            s = s.Replace("C", "Ց");
-            s = s.Replace("D", "Դ");
-            s = s.Replace("E", "Ե");
-            s = s.Replace("F", "Ֆ");
-            s = s.Replace("G", "Գ");
-            s = s.Replace("H", "Հ");
-            s = s.Replace("I", "Ի");
-            s = s.Replace("J", "Ջ");
-            s = s.Replace("K", "Կ");
-            s = s.Replace("L", "Լ");
-            s = s.Replace("M", "Մ");
-            s = s.Replace("N", "Ն");
-            s = s.Replace("O", "Օ");
-            s = s.Replace("P", "Պ");
-            s = s.Replace("Q", "Ք");
-            s = s.Replace("R", "Ռ");
-            s = s.Replace("S", "Ս");
-            s = s.Replace("T", "Տ");
-            s = s.Replace("U", "ՈՒ");
-            s = s.Replace("V", "Վ");
-            s = s.Replace("W", "Վ");
-            s = s.Replace("X", "Խ");
-            s = s.Replace("Y", "Յ");
-            s = s.Replace("Z", "Զ");
-            s = s.Replace("Ts", "Ծ");
-            s = s.Replace("Dz", "Ձ");
-            s = s.Replace("Kh", "Ղ");
-            s = s.Replace("Tj", "Ճ");
-            s = s.Replace("Sh", "Շ");
-            s = s.Replace("Vo", "Ո");
-            s = s.Replace("Ch", "Չ");
 
 
 
-
-            s = s.Replace("a", "ա");
-            s = s.Replace("b", "բ");
-            s = s.Replace("c", "ց");
-            s = s.Replace("d", "դ");
-            s = s.Replace("e", "ե");
-            s = s.Replace("f", "ֆ");
-            s = s.Replace("g", "գ");
-            s = s.Replace("h", "հ");
-            s = s.Replace("i", "ի");
-            s = s.Replace("j", "ջ");
-            s = s.Replace("k", "կ");
-            s = s.Replace("l", "լ");
-            s = s.Replace("m", "մ");
-            s = s.Replace("n", "ն");
-            s = s.Replace("o", "օ");
-            s = s.Replace("p", "պ");
-            s = s.Replace("q", "ք");
-            s = s.Replace("r", "ռ");
-            s = s.Replace("s", "ս");
-            s = s.Replace("t", "տ");
-            s = s.Replace("u", "ու");
-            s = s.Replace("v", "վ");
-            s = s.Replace("w", "վ");
-            s = s.Replace("x", "խ");
-            s = s.Replace("y", "յ");
-            s = s.Replace("z", "զ");
-            s = s.Replace("ts", "ծ");
-            s = s.Replace("dz", "ձ");
-            s = s.Replace("kh", "ղ");
-            s = s.Replace("tj", "ճ");
-            s = s.Replace("sh", "շ");
-            s = s.Replace("vo", "ո");
-            s = s.Replace("ch", "չ");
-
+            Console.WriteLine("Input the text!");
+            string s = Console.ReadLine();
             Console.WriteLine(s);
-            Console.ReadLine();
             Console.ReadKey();
+
+
 
 
         }
